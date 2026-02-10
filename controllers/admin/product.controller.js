ffadea3f8412253b7a4f7c50d767c69aba0ad82b
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-02-10 17:52:46 +0700

    Optimize the Pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,8 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
-
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 
@@ -28,20 +28,16 @@ module.exports.index = async (req, res) => {
     }
 
     // Pagination
-    let objectPagination = {
-        currentPage: 1,
-        limitItems: 4
-    }
-
-    if(req.query.page) {
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
     const countProducts = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
-    objectPagination.totalPage = totalPage;
+
+    let objectPagination = paginationHelper(
+        {
+            currentPage: 1,
+            limitItems: 4
+        },
+        req.query,
+        countProducts
+    )
     // End Pagination
 
     const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
