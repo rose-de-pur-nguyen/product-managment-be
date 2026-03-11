1c37c752c39ae05ea9fd1eb3343402b3f4d8af86
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-03-11 14:59:32 +1100

    Adjust the validation.redirectPath

--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -2,7 +2,7 @@ module.exports.createPost = (redirectPath) => {
     return (req, res, next) => {
         if(!req.body.title) {
             req.flash("error", "Vui lòng nhập tiêu đề!");
-            res.redirect("back");
+            res.redirect(redirectPath);
             return;
         };
 
