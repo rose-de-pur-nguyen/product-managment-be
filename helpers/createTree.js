7bca4851fcab32590252fe25935d781882654b5c
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-03-23 10:52:28 +1100

    Fix STT for category management page

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,16 +1,22 @@
-const createTree = (arr, parentId = "") => {
-    const tree = [];
-    arr.forEach((item) => {
-        if (item.parent_id === parentId) {
-            const newItem = item;
-            const children = createTree(arr, item.id);
-            if(children.length > 0) {
-                newItem.children = children;
+const createTree = (arr, parentId="") => {
+    let count = 0;
+    const Tree = (arr, parentId = "") => {
+        const tree = [];
+        arr.forEach((item) => {
+            if (item.parent_id === parentId) {
+                count++;
+                const newItem = item;
+                newItem.index = count;
+                const children = Tree(arr, item.id);
+                if(children.length > 0) {
+                    newItem.children = children;
+                }
+                tree.push(newItem);
             }
-            tree.push(newItem);
-        }
-    });
-    return tree;
+        });
+        return tree;
+    };
+    return Tree(arr, parentId);
 }
 
 module.exports = createTree;
