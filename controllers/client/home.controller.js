e963810d533cbbf68f0f1f77e925fd69550df819
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-04-06 23:37:54 +1000

    Display Latest Products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -15,11 +15,18 @@ module.exports.index = async (req, res) => {
     }).limit(3);
 
     const newProducts = productHelper.priceNewProducts(featuredRecords);
-
     // End Fetch Featured Products
+
+    // Fetch the Latest Products
+    const latestProducts = await Product.find({
+        status: "active",
+        deleted: false
+    }).sort({ position: "desc" }).limit(6);
+    // End Fetch the Latest Products
 
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
         featuredRecords: newProducts,
+        latestProducts: latestProducts
     });
 }
