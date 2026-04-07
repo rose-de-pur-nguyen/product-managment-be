26ece20e28f554778fbdbadb3dd99b4266205232
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-04-08 01:14:04 +1000

    miniCart button for header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -14,7 +14,18 @@ module.exports.cartId = async (req, res, next) => {
         
     } else {
         // Lấy giỏ hàng
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
 
+        // without initializing 0 as initial value for total
+        // on the first loop, total is not 0. It becomes the first element of cart.products
+        // this will returns an object
+        cart.totalQuantity = cart.products.reduce((total, item) => {
+            return total + item.quantity;
+        }, 0);
+
+        res.locals.miniCart = cart;
     }
 
     next();
