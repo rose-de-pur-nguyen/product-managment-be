e0354059ab4d45078775be96c09461f0dbc2d414
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-04-08 23:32:23 +1000

    Display cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,6 +1,41 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
 
+const productHelper = require("../../helpers/product");
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({ _id: cartId });
+
+    if(cart.products.length > 0) {
+        for(const item of cart.products) {
+            const productInfo = await Product.findOne(
+                { _id: item.product_id }
+            ).select("_id title thumbnail price discountPercentage slug");
+            
+            productInfo.priceNew = productHelper.priceNewProducts([productInfo])[0].priceNew;
+
+            item.productInfo = productInfo;
+
+            item.totalPrice = productInfo.priceNew * item.quantity;
+        }
+    }
+
+    const cartTotalPrice = cart.products.reduce((total, item) => {
+        return total + item.totalPrice;
+    }, 0);
+    cart.totalPrice = cartTotalPrice;
+    
+    
+    res.render("client/pages/cart/index.pug", {
+        pageTitle: "Giỏ hàng",
+        cart: cart
+    });
+}
+
+
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
     const productId = req.params.productId;
